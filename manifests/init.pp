530fd07b41aa651e9c2bf5d3ea249ebf5ba62412
Andulla <andreas.wilke@puppetlabs.com>
2016-09-26 16:49:27 +0200

    Updated manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,46 +1,17 @@
 # Class: vsphere_vm_hardening
 # ===========================
 #
-# Full description of class vsphere_vm_hardening here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'vsphere_vm_hardening':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
+# Puppet VMware vSphere Hardening Module to manage advanced parameters of your virtual machines.
 #
 # Authors
 # -------
 #
-# Author Name <author@domain.com>
+# Author Name <andreas@puppet.com>
 #
 # Copyright
 # ---------
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 Andreas Wilke.
 #
 
 class vsphere_vm_hardening (
